4bfc47e9878c893b37ab0bcd6d9748f32b73c01a
Max Ivashchenko <maxiivashenko@yandex.ru>
2025-12-05 18:39:46 +0300

    Add some key options for add subcommand

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,17 @@
 ﻿using System.CommandLine;
 
 RootCommand rootCommand = new("Command-line app for working with Active Directory users");
+
+var addCommand = new Command("add", "Add new user");
+
+var addCommandNameOption = new Option<string>("-n", "--name")
+{
+    Description = "User name"
+};
+
+var addCommandPasswordOptions = new Option<string>("-p", "password")
+{
+    Description = "User password"
+};
+
+return rootCommand.Parse(args).Invoke();
