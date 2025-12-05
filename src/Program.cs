cfdd661ab108f91a15319d2aa329cab04619017b
Max Ivashchenko <maxiivashenko@yandex.ru>
2025-12-05 17:27:19 +0300

    Add .editconfig

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+RootCommand rootCommand = new("Command-line app for working with Active Directory users");
